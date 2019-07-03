db0220f1017f29796206bb961e8bb338167c7863
elkingutierrex <elkin.gutierrex@gmail.com>
2019-07-03 16:30:18 -0500

    225 - Devolver listado de proyectos

--- routes/project.js
+++ routes/project.js
@@ -8,7 +8,8 @@ var router = express.Router();
 router.get('/home', ProjectController.home);
 router.post('/test', ProjectController.test);
 router.post('/save-project', ProjectController.saveProject);
-router.get('/project/:id?', ProjectController.getProject)
+router.get('/project/:id?', ProjectController.getProject);
+router.get('/projects', ProjectController.getProjects)
 
 
 
